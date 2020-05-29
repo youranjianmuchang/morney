514546da45b14eab0bfdc84e90fee5259b7c0abd
JieZhan <178889339@qq.com>
2020-05-29 21:17:41 +0800

    money.vue布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -18,6 +18,7 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 .nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -36,7 +37,7 @@ export default {};
     }
   }
   > .item.selected {
-    color: #ff9400;
+    color: $c-highlight;
   }
 }
 </style>
