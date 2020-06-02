5cb7f8540086f93d15dc21f48facb4a835d2cfe1
JieZhan <178889339@qq.com>
2020-06-02 23:24:55 +0800

    实现Notes组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,13 +2,19 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="请输入备注" />
+      <input type="text" placeholder="请输入备注" v-model="value" />
     </label>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Notes extends Vue {
+  value = "";
+}
 </script>
 
 <style lang="scss" scoped>
